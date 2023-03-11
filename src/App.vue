6fb6de2ec6f8e4325aa5f5a99223c25a75a2dac2
Alexander Byankin <samel@bk.ru>
2023-03-11 18:06:23 +0400

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Buffs from "@/components/Buffs.vue";
+</script>
+
+<template>
+    <Buffs />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Buffs.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    data() {
+        return {
+            buffs: [
+                {
+                    name: 'Bless',
+                    turns: {
+                        total: 40,
+                        left: 10,
+                    },
+                },
+                {
+                    name: 'Protection from Evil',
+                    turns: {
+                        total: 10,
+                        left: 10,
+                    },
+                },
+            ]
+        }
+    },
+}
+</script>
+
+<template>
+    <h1>Buffs</h1>
+    <div v-for="buff in buffs">
+        {{ buff.name }} {{buff.turns.left}}/{{buff.turns.total}}
+    </div>
+</template>
+
+<style scoped>
+
+</style>
